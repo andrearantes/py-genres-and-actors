d28cbe6d911beb265430c704c98c1cd460c29da3
Andre Marcio <andre.marcio.ti@gmail.com>
2026-06-13 15:01:46 -0300

    verify task

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from asyncio import create_task
-
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
@@ -9,15 +7,20 @@ from db.models import Genre, Actor
 
 def main() -> QuerySet:
     # Criar registros
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genres = ["Western", "Action", "Dramma"]
+    for name in genres:
+        Genre.objects.create(name=name)
+
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     genre = Genre.objects.get(name="Dramma")
     genre.name = "Drama"
